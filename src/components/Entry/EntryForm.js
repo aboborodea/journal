172aa791a882091a74e9e662e83ee3731f479fc9
aboborodea <aboborodea@gmail.com>
2019-12-04 16:00:58 -0500

    made changes to css

--- src/components/Entry/EntryForm.js
+++ src/components/Entry/EntryForm.js
@@ -7,31 +7,35 @@ const EntryForm = (props) => {
   return (
     <div className="container-fluid">
       <div className="entry-form">
-        <Form onSubmit={handleSubmit}>
+
+        <form onSubmit={handleSubmit}>
           <Form.Group controlId="title">
-            <Form.Control className="textbox-line1"
+            <Form.Control className="textbox"
               placeholder="Journal Title"
               value={entry.title}
               name="title"
               onChange={handleChange}
             />
           </Form.Group>
+
           <Form.Group controlId="author">
-            <Form.Control className="textbox-line1"
+            <Form.Control className="textbox"
               placeholder="01-01-2019"
               value={entry.date}
               name="date"
               onChange={handleChange}
             />
           </Form.Group>
+
           <form controlId="entry">
             <div className="textarea" value={entry.entry} name="entry" onChange={handleChange}>
-              <textarea>Whats on your mind today?</textarea>
+              <textarea></textarea>
             </div>
           </form>
+
           <Button className="entry-button" type="submit" variant="outline-info">Submit</Button>
           <Button className="entry-button" type="button" href={cancelPath} variant="outline-info">Cancel</Button>
-        </Form>
+        </form>
       </div>
     </div>
   )
